cb7452fb18b1ca1d94a4328ade7ddc017c142864
alchemyyy <github@grassmail.net>
2025-12-30 23:03:57 -0800

    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# MegaSlimeChunkFinder
-finds large slime chunk conglomerations in Minecraft
+Has some bugs at the moment, but works well enough.
+
+Uses AVX-512 and multithreading. Detects logical core count on host system to set # of threads.
+
+Outputs results periodically to a text file. Tracks progress and prints it out in stdout periodically.
---
+++ build.bat
@@ -0,0 +1,61 @@
+@echo off
+echo ========================================
+echo Slime Chunk Finder - Compilation Script
+echo ========================================
+echo.
+
+REM Find Visual Studio installation
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+
+if not exist "%VSWHERE%" (
+    echo ERROR: Visual Studio installer not found!
+    echo Please install Visual Studio 2022 with C++ tools.
+    pause
+    exit /b 1
+)
+
+REM Get VS installation path
+for /f "usebackq tokens=*" %%i in (`"%VSWHERE%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+    set "VSPATH=%%i"
+)
+
+if not defined VSPATH (
+    echo ERROR: Visual Studio C++ tools not found!
+    echo Please install Visual Studio 2022 with "Desktop development with C++" workload.
+    pause
+    exit /b 1
+)
+
+echo Found Visual Studio at: %VSPATH%
+echo.
+
+REM Setup Visual Studio environment
+call "%VSPATH%\VC\Auxiliary\Build\vcvars64.bat"
+
+echo.
+echo Compiling with AVX-512 optimizations...
+echo.
+
+REM Compile with maximum optimizations
+cl /std:c++17 /O2 /Oi /Ot /GL /arch:AVX512 /favor:INTEL64 /EHsc /nologo /Fe:slimechunkfinder.exe slimechunkfinder.cpp /link /LTCG
+
+if %ERRORLEVEL% EQU 0 (
+    echo.
+    echo ========================================
+    echo Compilation SUCCESSFUL!
+    echo ========================================
+    echo.
+    echo Executable: slimechunkfinder.exe
+    echo.
+    echo To run: slimechunkfinder.exe
+    echo Press Ctrl+C while running to see stats
+    echo.
+) else (
+    echo.
+    echo ========================================
+    echo Compilation FAILED!
+    echo ========================================
+    echo.
+)
+
+pause
